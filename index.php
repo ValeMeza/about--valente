dad43593756ea38ff18a06c6559533d271703b02
Adam Pedroza <apedroza6@cnm.edu>
2017-01-10 10:21:41 -0700

    added photo and finished

--- index.php
+++ index.php
@@ -4,15 +4,20 @@
     <title>About Valente</title>
 
 </head>
-<body>
-Hello
-My name is Valente Myson I am the father of seven known childeren.
+   <body>
+       <h2>
+           Hello
+       </h2>
+       <img src="image/pigeons.jpg" alt="pigeon">
+       <p>
+       My name is Valente Myson I am the father of seven known children.
 I was born in Brooklyn, New York and am most known for having an ear full in the ring while trying to stay out of prison.
 I have also played small roles in numerous movies such as Sacry Movie, Entourage, and The Hang Over.
 Besides fighting and acting I have a ssspecial love for animals, especailly pigeons.
 I decided to take this class as a second career in my early retirement.
+         </p>
 
 
 
-</body>
+   </body>
 </html>
